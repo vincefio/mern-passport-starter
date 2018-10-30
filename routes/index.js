931286eb912722ec4e40f759a640b04557f7b796
Vince <vincentfiorilli@gmail.com>
2018-10-30 05:51:18 -0400

    react added and user model defined

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,14 @@ router.get('/', function (req, res) {
     res.send('initial get route working');
 });
 
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        //req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
+
 
 module.exports = router;
