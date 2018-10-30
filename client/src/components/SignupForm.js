b41ae63906958d0803f28e5123497117cbe88b7f
Vince <vincentfiorilli@gmail.com>
2018-10-30 06:07:08 -0400

    something is working

---
+++ client/src/components/SignupForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SignupForm extends Component {
+    render() {
+        return (
+            <div>
+                signup form
+      </div>
+        )
+    }
+}
