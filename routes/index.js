0d7ba6adb4fb81f1f38d5d477f1436ca7b8191c9
Vince <vincentfiorilli@gmail.com>
2018-10-31 05:37:55 -0400

    ey yey yey

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User')
+var passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 // Get Homepage
 router.get('/', function (req, res) {
@@ -25,6 +28,70 @@ function ensureAuthenticated(req, res, next) {
         res.redirect('/users/login');
     }
 }
+
+router.post('/signup', (req, res) => {
+    const { username, password } = req.body
+    // ADD VALIDATION
+    User.findOne({ 'local.username': username }, (err, userMatch) => {
+        if (userMatch) {
+            return res.json({
+                error: `Sorry, already a user with the username: ${username}`
+            })
+        }
+        const newUser = new User({
+            'local.username': username,
+            'local.password': password
+        })
+        newUser.save((err, savedUser) => {
+            if (err) return res.json(err)
+            return res.json(savedUser)
+        })
+    })
+})
+
+const strategy = new LocalStrategy(
+    {
+        usernameField: 'username' // not necessary, DEFAULT
+    },
+    function (username, password, done) {
+        User.findOne({ 'local.username': username }, (err, userMatch) => {
+            if (err) {
+                return done(err)
+            }
+            if (!userMatch) {
+                return done(null, false, { message: 'Incorrect username' })
+            }
+            if (!userMatch.checkPassword(password)) {
+                return done(null, false, { message: 'Incorrect password' })
+            }
+            return done(null, userMatch)
+        })
+    }
+)
+
+// ==== Register Strategies ====
+passport.use(LocalStrategy)
+
+passport.serializeUser((user, done) => {
+    console.log('=== serialize ... called ===')
+    console.log(user) // the whole raw user object!
+    console.log('---------')
+    done(null, { _id: user._id })
+})
+
+passport.deserializeUser((id, done) => {
+    console.log('Deserialize ... called')
+    User.findOne(
+        { _id: id },
+        'firstName lastName photos local.username',
+        (err, user) => {
+            console.log('======= DESERILAIZE USER CALLED ======')
+            console.log(user)
+            console.log('--------------')
+            done(null, user)
+        }
+    )
+})
 
 
 module.exports = router;
